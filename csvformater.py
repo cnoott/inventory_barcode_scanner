2c8fd293376e4040123e7b90c5732b4a675fad37
Liam Amadio <liamamadio@gmail.com>
2020-01-17 17:22:13 -0600

    version 0.1

--- csvformater.py
+++
@@ -1,8 +0,0 @@
-#Written by Liam Amadio
-#01/15/2020
-import csv
-
-
-
-
-
--- inventory_scanner.py
+++ inventory_scanner.py
@@ -2,13 +2,14 @@
 #01/12/2020
 from collections import Counter
 import csv
+import datetime
 
 input_list = []
 num_of = []
 inventory_dict = {}
-
+print("Begin Scanning Items. Type enter when done: ")
 while True: #Puts user input into input_list
-    items = input("Begin Scanning Items. Type enter when done.")
+    items = input(">> ")
     if items == "":
         break
     input_list.append(items)
@@ -21,12 +22,14 @@ for id in input_list: #Counts qty of unique items in input_list
             if unique == id:
                 counter = counter + 1
         inventory_dict[str(id)] = counter
-print(inventory_dict)
+print("File saved at","inventory_{}".format(datetime.date.today()))
+print("barcode | quantity\n")
 
-file = open('output.csv',"w+")
+file = open('inventory_{}.csv'.format(datetime.date.today()),"w+")
 file.write("barcode,quantity\n")
 for keys in inventory_dict:
     file.write("{},{}\n".format(keys,inventory_dict[keys]))
+    print(keys,"      ",inventory_dict[keys])
             
         
 
