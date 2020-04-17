f2b7049d116facafef6d59076b8f60ec83e965ef
Liam Amadio <liamamadio@gmail.com>
2020-04-16 21:01:06 -0500

    tests

--- tests/tkinter_practice.py
+++ tests/tkinter_practice.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 root = tk.Tk()
 
-button = tk.Button(root)
+#constants
+HEIGHT = 700
+WIDTH = 800
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+button = tk.Button(root, text="BUDDON")
+button.pack()
 
 root.mainloop()
